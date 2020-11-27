df439fd698c63df9a48e0de131fa53a93d3fb629
dhanyaal <dh11any@hotmail.com>
2020-11-27 23:05:50 +0000

    angular-catalog-cart-and-shipping

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { CartService } from "../cart.service";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
+})
+export class CartComponent implements OnInit {
+  items;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
+}
